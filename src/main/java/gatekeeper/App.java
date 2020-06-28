cbbd12d5915d397dca441000c94c3d05c6051564
Martin Straus <martin.straus@fit.com.ar>
2020-06-27 22:10:03 -0300

    Hello world with packaging.

--- src/main/java/gatekeeper/App.java
+++
@@ -1,8 +0,0 @@
-package gatekeeper;
-
-public class App {
-
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
-}
---
+++ src/main/kotlin/App.kt
@@ -0,0 +1,5 @@
+package gatekeeper
+
+fun main(args: Array<String>) {
+    println("Hello World!")
+}
